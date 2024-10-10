4822622c6bb223a98ccea0b3ec442c373ddc6277
Felipe Bernardo de Oliveira <felipebernardo55@hotmail.com>
2024-10-10 16:56:19 -0300

    feat criando teste unitário para controller Team

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 <h1 align="center">Teste Técnico Irroba</h1>
-<h3>Configurações de ambiente para utilizar a aplicação:</h3>
-<p>PHP: 7.4.33</p>
-<p>Laravel: 8.83.27</p>
-<p>Database: MySQL</p>
+<h3>Configurações de ambiente:</h3>
+<p>- Utilizar versão PHP: 7.4.33</p>
+<p>- Utilizar versão Laravel: 8.83.27</p>
+<p>- Utilizar banco de dados: MySQL</p>
+<h3>Passo a passo para instalar e utilizar a aplicação:</h3>
+<p>Após instalar as versões requiridas e clonar o projeto, executar o comando "composer install" dentro do projeto</p>
 <p>Criar banco de dados usando MySQL no ambiente em que for testar aplicação e configurar o arquivo .env do projeto</p>
-<p>Comando para executar migration das tabelas do projeto: 'php artisan migrate'</p>
-<p>Comando para executar o projeto: 'php artisan serve'</p>
+<p>Executar comando para criar as tabelas no banco de dados configurado usando a migration da aplicação: "php artisan migrate"</p>
+<p>Executar comando para servir o projeto localmente: 'php artisan serve'</p>
 <br>
-<p>Documentação Collection Postman</p>
+<p>Documentação Collection Postman:</p>
 <a href="https://www.postman.com/altimetry-specialist-72965033/teste-irroba/overview">Link Collection</a>
---
+++ tests/Unit/TeamsControllerTest.php
@@ -0,0 +1,165 @@
+<?php
+
+use Illuminate\Http\Request;
+use Tests\TestCase;
+use App\Http\Controllers\TeamsController;
+use App\Models\Teams;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Mockery\MockInterface;
+
+class TeamsControllerTest extends TestCase
+{
+    public function test_responseOkMethod(): void
+    {
+        $mockModel = Mockery::mock(Teams::class);
+        $mockController = new TeamsController($mockModel);
+
+        $mockResponse = ["msg" => "Response OK"];
+        $response = $mockController->responseOK($mockResponse);
+
+        $this->assertEquals(200, $response->status());
+        $this->assertJsonStringEqualsJsonString(
+            json_encode($mockResponse),
+            $response->getContent()
+        );
+    }
+
+    public function test_errorMethod(): void
+    {
+        $mockModel = Mockery::mock(Teams::class);
+        $mockController = new TeamsController($mockModel);
+
+        $mockMessage = 'Erro ao fazer requisicao';
+        $response = $mockController->error($mockMessage);
+
+        $this->assertEquals(404, $response->status());
+        $this->assertJsonStringEqualsJsonString(
+            json_encode(['error' => $mockMessage]),
+            $response->getContent()
+        );
+    }
+
+    public function test_readTeamIdExpectOk(): void
+    {
+        $id = 1;
+        $mockResponse = ['teste'];
+
+        $mockModel = Mockery::mock(Teams::class);
+        $mockModel->shouldReceive('readTeamId')
+            ->with($id)
+            ->once()
+            ->andReturn($mockResponse);
+
+        $mockController = new TeamsController($mockModel);
+        $response = $mockController->readTeamId($id);
+
+        $this->assertEquals(200, $response->status());
+        $this->assertJsonStringEqualsJsonString(
+            json_encode($mockResponse),
+            $response->getContent()
+        );
+    }
+
+    public function test_readTeamsExpectOk(): void
+    {
+        $mockResponse = ['teste', 'teste2'];
+
+        $mockModel = Mockery::mock(Teams::class);
+        $mockModel->shouldReceive('readTeams')
+            ->once()
+            ->andReturn($mockResponse);
+
+        $mockController = new TeamsController($mockModel);
+        $response = $mockController->readTeams();
+
+        $this->assertEquals(200, $response->status());
+        $this->assertJsonStringEqualsJsonString(
+            json_encode($mockResponse),
+            $response->getContent()
+        );
+    }
+
+    public function test_createTeamsExpectOk(): void
+    {
+        $mockRequest = new Request([
+            "teams" => [
+                "time1",
+                "time2"
+            ]
+        ]);
+        $mockModel = Mockery::mock(Teams::class);
+        $mockModel->shouldReceive('createTeams')
+            ->with($mockRequest['teams'])
+            ->once()
+            ->andReturn(true);
+        $mockController = new TeamsController($mockModel);
+        $response = $mockController->createTeams($mockRequest);
+
+        $this->assertEquals(200, $response->status());
+        $this->assertJsonStringEqualsJsonString(
+            json_encode(['msg' => 'Times registrados com sucesso!']),
+            $response->getContent()
+        );
+    }
+
+    public function test_readTeamIdExpectError(): void
+    {
+        $id = 1;
+        $mockResponse = [];
+
+        $mockModel = Mockery::mock(Teams::class);
+        $mockModel->shouldReceive('readTeamId')
+            ->with($id)
+            ->once()
+            ->andReturn($mockResponse);
+
+        $mockController = new TeamsController($mockModel);
+        $response = $mockController->readTeamId($id);
+
+        $this->assertEquals(404, $response->status());
+        $this->assertJsonStringEqualsJsonString(
+            json_encode(['error' => 'Time não encontrado, tente novamente mais tarde!']),
+            $response->getContent()
+        );
+    }
+
+    public function test_readTeamsExpectError(): void
+    {
+        $mockResponse = [];
+
+        $mockModel = Mockery::mock(Teams::class);
+        $mockModel->shouldReceive('readTeams')
+            ->once()
+            ->andReturn($mockResponse);
+
+        $mockController = new TeamsController($mockModel);
+        $response = $mockController->readTeams();
+
+        $this->assertEquals(404, $response->status());
+        $this->assertJsonStringEqualsJsonString(
+            json_encode(['error' => 'Times não encontrados, tente novamente mais tarde!']),
+            $response->getContent()
+        );
+    }
+
+    public function test_createTeamsExpectError(): void
+    {
+        $mockRequest = new Request([
+            "teams" => []
+        ]);
+
+        $mockModel = Mockery::mock(Teams::class);
+        $mockModel->shouldReceive('createTeams')
+            ->with($mockRequest['teams'])
+            ->once()
+            ->andReturn(false);
+        $mockController = new TeamsController($mockModel);
+        $response = $mockController->createTeams($mockRequest);
+
+        $this->assertEquals(404, $response->status());
+        $this->assertJsonStringEqualsJsonString(
+            json_encode(['error' => 'Times não puderam ser cadastrados, tente novamente mais tarde!']),
+            $response->getContent()
+        );
+    }
+}
